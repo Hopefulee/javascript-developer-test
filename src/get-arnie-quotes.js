f4f78bc2081deac5b798a6e31f6b78a3f3838a69
Parry Lee <parry.lee@hotmail.com>
2025-05-31 22:45:32 +1000

    simplify getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,30 +1,16 @@
 const { httpGet } = require('./mock-http-interface');
 
+const mapResponse = response => {
+  const responseBody = JSON.parse(response.body);
+  return response?.status === 200 ? { 'Arnie Quote': responseBody.message } : { 'FAILURE': responseBody.message };
+};
+
 const getArnieQuotes = async urls => {
   if (!urls || urls.length === 0) {
     return [];
   }
 
-  const results = [];
-
-  // Process each URL
-  const promises = urls?.map(async url => {
-    const response = await httpGet(url);
-    const responseBody = JSON.parse(response.body);
-
-    if (response.status === 200) {
-      results.push({ 'Arnie Quote': responseBody.message });
-    } else {
-      results.push({ 'FAILURE': responseBody.message });
-    }
-  });
-
-  if (promises?.length) {
-    // Wait for all requests to complete
-    await Promise.all(promises);
-  }
-
-  return results;
+  return (await Promise.all(urls.map(url => httpGet(url))))?.map(response => mapResponse(response));
 };
 
 module.exports = {
