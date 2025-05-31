90eb5884c3c5048ba0a33da838254e1ada7278a7
Parry Lee <parry.lee@hotmail.com>
2025-05-31 21:01:10 +1000

    Execute a HTTP GET If the HTTP status code of the response is 200, push an object to the results array with a single key "Arnie Quote" and the HTTP response body's message property as the key's associated value. If the HTTP status code of the response is not 200, push an object to the results array with a single key "FAILURE" and the HTTP response body's message property as the key's associated value.

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,26 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = [];
+
+  // Process each URL
+  const promises = urls?.map(async url => {
+    const response = await httpGet(url);
+    const responseBody = JSON.parse(response.body);
+
+    if (response.status === 200) {
+      results.push({ 'Arnie Quote': responseBody.message });
+    } else {
+      results.push({ 'FAILURE': responseBody.message });
+    }
+  });
+
+  if (promises?.length) {
+    // Wait for all requests to complete
+    await Promise.all(promises);
+  }
+
+  return results;
 };
 
 module.exports = {
